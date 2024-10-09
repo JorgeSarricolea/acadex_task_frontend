88573edae3e5aaae73cd31c3e20c7aa53ae0bf1e
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-08 23:16:38 -0600

    feat(entity): add token attribute to User class
        - Updated 'User' class constructor to include 'token' attribute.
        - Ensured optional handling for 'lastName' attribute.

--- src/app/domain/entities/User.js
+++ src/app/domain/entities/User.js
@@ -1,8 +1,9 @@
 export class User {
-  constructor({ id, firstName, lastName, email }) {
+  constructor({ id, firstName, lastName, email, token }) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName || ""; // Optional
     this.email = email;
+    this.token = token;
   }
 }
