9f1be2ed1b047705b52d1121dcd8311f044767f2
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-08 23:14:38 -0600

    feat(api): add validateToken endpoint
        - Added 'validateToken' endpoint to the API configuration for token validation.
        - Updated BASE_URL to include the validate-token route in the endpoints object.

--- src/app/infrastructure/config/configAPI.js
+++ src/app/infrastructure/config/configAPI.js
@@ -3,6 +3,7 @@ const BASE_URL = "http://localhost:8000/api/v1";
 const endpoints = {
   signup: `${BASE_URL}/auth/signup`,
   login: `${BASE_URL}/auth/login`,
+  validateToken: `${BASE_URL}/auth/validate-token`,
   getUser: (userId) => `${BASE_URL}/users/${userId}`,
 };
 
