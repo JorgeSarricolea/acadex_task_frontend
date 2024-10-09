86f96044fde59b6f37c6a9e96ed838d79329f697
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-08 23:22:32 -0600

    feat(home): implement user greeting with 'useAuth' hook
        - Integrated 'useAuth' hook to fetch and display the logged-in user's email.
        - Added a welcome message that greets the user upon successful login.
        - Included a loading state while the authentication check is being performed.

--- src/app/pages/home/page.jsx
+++ src/app/pages/home/page.jsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Hello, world from Home</div>;
+"use client";
+
+import { useAuth } from "@/app/hooks/useAuth";
+
+function Home() {
+  const { userEmail } = useAuth();
+
+  return (
+    <div>{userEmail ? <h1>Welcome {userEmail}!</h1> : <p>Loading...</p>}</div>
+  );
 }
+
+export default Home;
