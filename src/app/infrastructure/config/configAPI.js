198d412665460f3457b5513ae5ee5de366049119
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-10 11:35:31 -0600

    feat(configAPI): add user-related API endpoints
        - Introduced 'getUserById' and 'updateUser' methods to the API configuration.
        - These methods facilitate fetching and updating user data based on user ID.
        - Improved clarity by organizing user-related endpoints under comments.

--- src/app/infrastructure/config/configAPI.js
+++ src/app/infrastructure/config/configAPI.js
@@ -4,7 +4,10 @@ const endpoints = {
   signup: `${BASE_URL}/auth/signup`,
   login: `${BASE_URL}/auth/login`,
   validateToken: `${BASE_URL}/auth/validate-token`,
-  getUser: (userId) => `${BASE_URL}/users/${userId}`,
+
+  // Users
+  getUserById: (userId) => `${BASE_URL}/users/${userId}`,
+  updateUser: (userId) => `${BASE_URL}/users/${userId}`,
 };
 
 export default endpoints;
