1f6e5bcd3bb8249b6c51c5c73a95fec4bded5afa
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-10 11:36:24 -0600

    refactor(useAuth): improve token validation logic
        - Restructured token validation to first retrieve the token from sessionStorage.
        - Redirects to login if the token is missing or invalid, enhancing authentication flow.
        - Improved code readability by organizing imports and updating validation checks.

--- src/app/hooks/useAuth.js
+++ src/app/hooks/useAuth.js
@@ -1,9 +1,10 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { validateToken } from "@/app/application/services/AuthService.js";
-import { getUserEmail } from "@/app/application/services/StorageService.js";
+import {
+  getUserEmail,
+  getToken,
+} from "@/app/application/services/StorageService.js";
 
 export const useAuth = () => {
   const [userEmail, setUserEmail] = useState(null);
@@ -11,8 +12,17 @@ export const useAuth = () => {
 
   useEffect(() => {
     const checkAuth = async () => {
-      const isValid = await validateToken();
-      if (!isValid) {
+      const token = getToken(); // Get token from sessionStorage
+
+      if (!token) {
+        router.push("/login");
+        return;
+      }
+
+      const validationResponse = await validateToken(token); // Validate token
+
+      // Ensure you're checking the correct response structure (isValid)
+      if (!validationResponse || !validationResponse.isValid) {
         router.push("/login");
       } else {
         const email = getUserEmail();
